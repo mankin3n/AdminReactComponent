de2e177a506b475a64fff5b7e39967793a09d03a
Pauli Mankinen <68276769+mankin3n@users.noreply.github.com>
2022-03-07 15:50:47 +0200

    nice

--- app/app.js
+++
@@ -1,9 +0,0 @@
-import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
-
-const handleRequest = (request) => {
-  console.log(`Request to ${request.url}`);
-  return new Response("Hello world!");
-};
-
-console.log("Launching server on port 7777");
-serve(handleRequest, { port: 7777 });
